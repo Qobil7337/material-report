ac5096e99065076db5bd72b988f5876dce130ca3
Qobil7337 <qobil7337@mail.ru>
2023-11-03 15:48:19 +0500

    work in calculation and goods inwards of front is done for now

--- src/app/calculation/calculation.component.ts
+++ src/app/calculation/calculation.component.ts
@@ -6,5 +6,115 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculation.component.css']
 })
 export class CalculationComponent {
-    calculation: any
+    selectedCalculation: any
+    calculation= [
+      {
+        calculationID: 1,
+        productID: 1,
+        productName: "Chicken sandwich",
+        dataCreated: "2023-10-01",
+        nomenclatures: [
+          {
+            nomenclatureID: "6",
+            nomenclatureName: "Sandwich bread",
+            unitOfMeasure: "pc",
+            quantity: "1",
+            price: "3 000",
+            totalSum: "3 000"
+          },
+          {
+            nomenclatureID: "7",
+            nomenclatureName: "Chicken",
+            unitOfMeasure: "kg",
+            quantity: "0.2",
+            price: "30 000",
+            totalSum: "6 000"
+          },
+          {
+            nomenclatureID: "8",
+            nomenclatureName: "Tomato",
+            unitOfMeasure: "kg",
+            quantity: "0.05",
+            price: "15 000",
+            totalSum: "750"
+          },
+          {
+            nomenclatureID: "9",
+            nomenclatureName: "Salad",
+            unitOfMeasure: "kg",
+            quantity: "0.02",
+            price: "35 000",
+            totalSum: "700"
+          },
+          {
+            nomenclatureID: "10",
+            nomenclatureName: "Tomato Sauce",
+            unitOfMeasure: "litre",
+            quantity: "0.02",
+            price: "50 000",
+            totalSum: "1000"
+          }
+        ],
+        totalCost: "11 450",
+        grossProfit: "23 550",
+        salePrice: "35 000",
+        grossProfitMargin: "67%"
+      },
+      {
+        calculationID: 1,
+        productID: 1,
+        productName: "Beef sandwich",
+        dataCreated: "2023-10-02",
+        nomenclatures: [
+          {
+            nomenclatureID: "6",
+            nomenclatureName: "Sandwich bread",
+            unitOfMeasure: "pc",
+            quantity: "1",
+            price: "3 000",
+            totalSum: "3 000"
+          },
+          {
+            nomenclatureID: "7",
+            nomenclatureName: "Beef",
+            unitOfMeasure: "kg",
+            quantity: "0.2",
+            price: "60 000",
+            totalSum: "12 000"
+          },
+          {
+            nomenclatureID: "8",
+            nomenclatureName: "Tomato",
+            unitOfMeasure: "kg",
+            quantity: "0.05",
+            price: "15 000",
+            totalSum: "750"
+          },
+          {
+            nomenclatureID: "9",
+            nomenclatureName: "Salad",
+            unitOfMeasure: "kg",
+            quantity: "0.02",
+            price: "35 000",
+            totalSum: "700"
+          },
+          {
+            nomenclatureID: "10",
+            nomenclatureName: "Tomato Sauce",
+            unitOfMeasure: "litre",
+            quantity: "0.02",
+            price: "50 000",
+            totalSum: "1000"
+          }
+        ],
+        totalCost: "17 450",
+        grossProfit: "27 550",
+        salePrice: "45 000",
+        grossProfitMargin: "61%"
+      }
+    ]
+
+  openModal(item: any) {
+      this.selectedCalculation = item
+  }
 }
--- src/app/goods-inward/goods-inward.component.ts
+++ src/app/goods-inward/goods-inward.component.ts
@@ -7,4 +7,91 @@ import { Component } from '@angular/core';
 })
 export class GoodsInwardComponent {
 
+  selectedGoodsInwardsItem: any
+
+  goodsInwards = [
+    {
+      goodsInwardsID: 1,
+      supplier: "Mall",
+      warehouse: "Central",
+      totalSum: "60 000",
+      dateOfDelivery: "2023-10-01",
+      goodsInwardsItem: [
+        {
+          goodsInwardsID: 1,
+          nomenclatureID: "2",
+          nomenclatureName: "Tomato",
+          unitOfMeasure: "kg",
+          quantity: 1.5,
+          price: "10 000",
+          totalSum: "15 000",
+          dateOfDelivery: "2023-10-01"
+        },
+        {
+          goodsInwardsID: 1,
+          nomenclatureID: "3",
+          nomenclatureName: "Cheese",
+          unitOfMeasure: "kg",
+          quantity: 1,
+          price: "15 000",
+          totalSum: "15 000",
+          dateOfDelivery: "2023-10-01"
+        },
+        {
+          goodsInwardsID: 1,
+          nomenclatureID: "4",
+          nomenclatureName: "Sandwich bread",
+          unitOfMeasure: "pc",
+          quantity: 15,
+          price: "20 00",
+          totalSum: "30 000",
+          dateOfDelivery: "2023-10-01"
+        }
+      ]
+    },
+    {
+      goodsInwardsID: 2,
+      supplier: "Mall",
+      warehouse: "Central",
+      totalSum: "90 000",
+      dateOfDelivery: "2023-10-02",
+      goodsInwardsItem: [
+        {
+          goodsInwardsID: 2,
+          nomenclatureID: "6",
+          nomenclatureName: "Potato",
+          unitOfMeasure: "kg",
+          quantity: 3,
+          price: "10 000",
+          totalSum: "30 000",
+          dateOfDelivery: "2023-10-02"
+        },
+        {
+          goodsInwardsID: 2,
+          nomenclatureID: "4",
+          nomenclatureName: "Cheese",
+          unitOfMeasure: "kg",
+          quantity: 2,
+          price: "20 000",
+          totalSum: "40 000",
+          dateOfDelivery: "2023-10-02"
+        },
+        {
+          goodsInwardsID: 2,
+          nomenclatureID: "6",
+          nomenclatureName: "Sandwich bread",
+          unitOfMeasure: "pc",
+          quantity: 10,
+          price: "2 000",
+          totalSum: "20 000",
+          dateOfDelivery: "2023-10-02"
+        }
+      ]
+    }
+  ]
+
+  openModal(item: any) {
+    this.selectedGoodsInwardsItem = item
+    }
+
 }
